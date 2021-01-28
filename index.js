c15b14f079be71b2229069a48fb7d83c52991c60
KonghaYao <20192831006@m.scnu.edu.cn>
2021-01-28 20:48:02 +0800

    2021/1/28

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fetch = require("node-fetch");
 const fs = require("fs-extra");
-fetch("https://www.baidu.com")
+fetch("https://rsshub.app/pixiv/ranking/week")
     .then((res) => res.text())
     .then((res) => {
         fs.writeFile("./dist/file.html", res);
+        console.log(res);
         console.log("保存完成");
     });
