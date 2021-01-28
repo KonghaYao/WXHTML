93963ddd12d24d1c4a429b9d862794c99a5aec3e
KonghaYao <20192831006@m.scnu.edu.cn>
2021-01-28 21:18:45 +0800

    2021/1/28

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const fetch = require("node-fetch");
-const fs = require("fs-extra");
-fetch("https://rsshub.app/pixiv/ranking/week")
-    .then((res) => res.text())
-    .then((res) => {
-        fs.writeFile("./dist/file.html", res);
-        console.log(res);
-        console.log("保存完成");
-    });
+const AV = require("leancloud-storage");
+AV.init({
+    appId: "0ER6ub3jKGsff0bDnvcE3InS-gzGzoHsz",
+    appKey: "U017BnxwoBNCxk0ls4Rxyzhl",
+    serverURL: "https://0er6ub3j.lc-cn-n1-shared.com/",
+});
+const websites = [
+    {
+        name: "P站",
+        url: "https://rsshub.app/pixiv/ranking/week",
+    },
+];
+websites.forEach((i) => {
+    fetch(i.url)
+        .then((res) => res.text())
+        .then((content) => {
+            const RSS = AV.Object.extend("RSS");
+            const todo = new RSS();
+            todo.set("name", i.name);
+            todo.set("fromWebsite", i.url);
+            todo.set("content", content);
+            // 将对象保存到云端
+            todo.save().then(
+                (todo) => {
+                    // 成功保存之后，执行其他逻辑
+                    console.log(`保存成功。objectId：${todo.id}`);
+                },
+                (error) => {
+                    console.log("错误");
+                }
+            );
+            console.log("保存完成");
+        });
+});
