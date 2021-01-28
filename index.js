3c86727d7b35df567bdc868a5af2a737ebe85af6
KonghaYao <20192831006@m.scnu.edu.cn>
2021-01-28 19:34:11 +0800

    2021/1/28

---
+++ index.js
@@ -0,0 +1,8 @@
+const fetch = require("node-fetch");
+const fs = require("fs-extra");
+fetch("https://www.baidu.com")
+    .then((res) => res.text())
+    .then((res) => {
+        fs.writeFile("./file.html", res);
+        console.log("保存完成");
+    });
